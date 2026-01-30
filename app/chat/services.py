f533922052427a663f3a16a01cbd1a768e22e816
Sheftel <sheftel.ks@gmail.com>
2026-01-30 21:12:04 +0700

    Add services

---
+++ app/chat/services.py
@@ -0,0 +1,32 @@
+from chat.models import Chat, Message
+
+
+def chat_create(
+    title: str
+) -> Chat:
+
+    chat = Chat(title=title)
+    chat.full_clean()
+    chat.save()
+
+    return chat
+
+
+def message_create(
+    chat_id: int,
+    text: str,
+) -> Message:
+    message = Message(chat_id=chat_id, text=text)
+    message.full_clean()
+    message.save()
+
+    return message
+
+
+def chat_delete(
+    chat: Chat,
+) -> None:
+
+    chat.delete()
+
+    return None
