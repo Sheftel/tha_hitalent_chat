d86931d32b59410bcd4186edf221d0c97d15f55a
Sheftel <sheftel.ks@gmail.com>
2026-01-30 22:15:09 +0700

    Add string repr to models

--- app/chat/models/chat.py
+++ app/chat/models/chat.py
@@ -7,4 +7,5 @@ class Chat(BaseModel):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200, blank=False) #TODO: trim whitespaces from both ends
 
-
+    def __str__(self):
+        return 'Chat %d: %s' % (self.id, self.title)
--- app/chat/models/message.py
+++ app/chat/models/message.py
@@ -7,3 +7,6 @@ class Message(BaseModel):
     id = models.AutoField(primary_key=True)
     text = models.TextField(max_length=5000, blank=False)
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages')
+
+    def __str__(self):
+        return 'Message %d: %s' % (self.id, self.text)
