88f251b08680313d11b9d2bbf817fb73496a6a3a
Sheftel <sheftel.ks@gmail.com>
2026-01-30 22:15:25 +0700

    Update chat selector

--- app/chat/selectors.py
+++ app/chat/selectors.py
@@ -1,9 +1,10 @@
-from chat.models import Chat
+from django.db.models import Prefetch
 
+from chat.models import Chat, Message
 
 
 def chat_retrieve(
     chat_id: int
 ) -> Chat:
-    return Chat.object.get(id=chat_id).prefetch_related('messages').first()
-
+    message_prefetch = Prefetch('messages', queryset=Message.objects.order_by('-created_at'))
+    return Chat.objects.filter(id=chat_id).prefetch_related(message_prefetch).first()
